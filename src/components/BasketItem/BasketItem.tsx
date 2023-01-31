c62f723e856b457a19e7f7f1879da199c5cd21ef
alademerin <davidalademerin@gmail.com>
2023-01-31 19:41:17 +0100

    add BasketItem component files

---
+++ src/components/BasketItem/BasketItem.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BasketItem = () => {
+  return (
+    <View>
+      <Text>BasketItem</Text>
+    </View>
+  )
+}
+
+export default BasketItem
