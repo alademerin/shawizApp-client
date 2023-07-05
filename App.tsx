20e9e749a2dff844d37df618907a0aece35a6d3d
davidalademerin <david@simplifytechnologies.io>
2023-07-05 18:13:56 +0100

    setup scaffold for apolloClient authLink

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import HomeScreen from "./src/screens/HomeScreen/HomeScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeStack from "./src/components/Navigation";
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, createHttpLink } from "@apollo/client";
 import { store } from "./src/store/store.js";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import AppContainer from "./src/screens/AppContainer";
 import { Toast } from "react-native-toast-message/lib/src/Toast";
+import * as SecureStore from "expo-secure-store";
+import { setContext } from "@apollo/client/link/context";
 
-const Stack = createNativeStackNavigator();
+const httpLink = createHttpLink({
+  uri: "http://172.20.10.3:5000/graphql",
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const authorization = await SecureStore.getItemAsync("token");
+  return {
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${authorization}`,
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "http://172.20.10.3:5000/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -24,7 +32,7 @@ export default function App() {
       <ApolloProvider client={client}>
         <AppContainer />
       </ApolloProvider>
-      <Toast/>
+      <Toast />
     </Provider>
   );
 }
